198341e09ee93e22f7128c4b05aab11303bdf84f
arthur <arthurysiite@gmail.Com>
2019-08-22 20:15:36 +0200

    hypertube

--- class/init.class.php
+++ class/init.class.php
@@ -3,10 +3,30 @@
 Class init {
 
 	private $modules = [];
+	private $nb_modules = 0;
 
 	public function __Construct() {
 
 		$this->get_modules();
+		$this->compare_modules();
+	}
+
+	private function compare_modules() {
+
+		$path = new link('.env_modules');
+		$file = $path->get_link(1);
+		if (count(file($file)) != $this->nb_modules)
+			$this->load_modules($file);
+	}
+
+	private function load_modules($file) {
+
+		file_put_contents($file, "");
+		$i = 0;
+		while (isset($this->modules[$i])) {
+			file_put_contents($file, serialize($this->modules[$i]));
+			$i++;
+		}
 	}
 
 	private function get_modules() {
@@ -23,9 +43,8 @@ Class init {
 
 			$data = $this->infos_module($filename);
    			$this->modules[][$data['name_module']] = $data['infos_module'];
+   			$this->nb_modules++;
 		}
-
-		return ($this->modules);
 	}
 
 	private function infos_module($link_module) {
@@ -45,7 +64,7 @@ Class init {
 		while (isset($cat_file[$i])) {
 
 			if (preg_match('#commandes = #', $cat_file[$i])) {
-				
+
 				preg_match_all('#(\[[^\]]*\])#', $cat_file[$i], $matches);
 				$matches[0][0] = str_replace("[", "", $matches[0][0]);
 				$matches[0][0] = str_replace("]", "", $matches[0][0]);
