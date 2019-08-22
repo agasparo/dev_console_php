473548ba63c7056f364c139e5e59792280cf34ef
Arthur GASPAROTTO <agasparo@e1r7p8.42.fr>
2019-08-22 17:17:23 +0200

    hypertube

--- js/console.js
+++ js/console.js
@@ -5,7 +5,7 @@ function init() {
 	input_console.onkeydown = function(e) {
 		if (e.which == 13) {
 			e.preventDefault();
-			$.post('../dev_console/console.php', {commande:input_console.value}, function(data) {
+			$.post('http://192.168.99.100.xip.io:41062/www/dev_console/console.php', {commande:input_console.value}, function(data) { // chnager url ici
 				input_console.remove();
 				let text = '>><<clear';
 				match = data.match(text);
@@ -14,7 +14,7 @@ function init() {
 				if (match)
 					document.getElementById('conbsole_b').innerHTML = e[1].replace('clear', '');
 				else if (com[0] == "set")
-					window.location = "http://192.168.99.100.xip.io:41062/www/hypertube/";
+					window.location = "http://192.168.99.100.xip.io:41062/www/hypertube/"; //changer ici
 				else
 					$("#conbsole_b").append(data);
 				document.getElementById('conbsole_b').scrollTop = document.getElementById('conbsole_b').scrollHeight
