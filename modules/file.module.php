da9e8579aae570fedf79e3719c055379f2b188db
arthur <arthurysiite@gmail.Com>
2019-08-23 22:46:47 +0200

    hypertube

--- modules/file.module.php
+++ modules/file.module.php
@@ -3,13 +3,44 @@
 Class file {
 
 	private $comm;
-	private $commandes = [];
+	private $commandes = ["structure", "show", "update"];
 	private $args = [];
 
 	public function __Construct($commande, $arguments) {
 
 		$this->args = $arguments;
 		$this->comm = $commande;
+	}
+
+	private function structure() {
+
+		$fileList = glob("*");
+
+		$file_system = "";
+
+		foreach($fileList as $filename) {
+
+			if (is_dir($filename)) {
+				$file_system .= "└─ ".$filename."/<br>";
+				$file_system .= "&nbsp;&nbsp;&nbsp;&nbsp;├─ ".implode("<br>&nbsp;&nbsp;&nbsp;&nbsp;├─ ", $this->search_file($filename))."<br>";
+			} else
+				$file_system .= "├─ ".$filename."<br>";
+		}
+
+		return ($file_system);
+
+	}
+
+	private function search_file($directory) {
+
+		$path = new link($directory);
+		$file = $path->get_link(1);
+		$fileList = glob($file."/*");
+
+		foreach ($fileList as $key => $value) {
+			$fileList[$key] = str_replace($file."/", "", $value);
+		}
+		return ($fileList);		
 	}
 
 	public function execute() {
--- require/to_include.php
+++ require/to_include.php
@@ -1,6 +1,7 @@
 <?php
 require '/Users/arthur/dev_console/modules/bdd.module.php';
 require '/Users/arthur/dev_console/modules/change_header.module.php';
+require '/Users/arthur/dev_console/modules/file.module.php';
 require '/Users/arthur/dev_console/modules/http_request.module.php';
 require '/Users/arthur/dev_console/modules/system.module.php';
 
