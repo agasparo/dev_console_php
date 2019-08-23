5bb0710c225826796b63617447103043f0b1e8c5
arthur <arthurysiite@gmail.Com>
2019-08-23 15:53:01 +0200

    hypertube

--- modules/bdd.module.php
+++ modules/bdd.module.php
@@ -5,6 +5,7 @@ Class bdd {
 	private $args = [];
 	private $comm;
 	private $bdd;
+	private $tables = [];
 	private $commandes = ["insert", "update", "delete", "show_table", "get"];
 
 	public function __Construct($commande, $arguments) {
@@ -23,21 +24,62 @@ Class bdd {
 		$data[3] = str_replace("\n", "", $data[3]);
 
 		$this->bdd = new PDO(str_replace("\n", "", $data[0]), str_replace("\n", "", $data[1]), $data[2]);
+
+		$req_table = $this->bdd->query("SHOW TABLES");
+		$res = $req_table->fetchAll();
+		$this->tables = [];
+
+		$i = 0;
+		while (isset($res[$i])) {
+			$this->tables[] = $res[$i][0];
+			$i++;
+		}
 	}
 
 	private function show_table() {
 
-		$req_table = $this->bdd->query("SHOW TABLES");
-		$res = $req_table->fetchAll();
-		$tables = [];
+		return ($this->create_table("Database table(s)", $this->tables, 0));
+	}
 
+	private function get() {
+
+		if (!isset($this->args[0]))
+			return ("Usage : bdd.get [table to get infos]");
+
+		if (!in_array($this->args[0], $this->tables))
+			return ("Table ".$this->args[0]." doesn't exist ");
+
+		$get_infs = $this->bdd->query('DESCRIBE '.$this->args[0]);
+		$colum = $get_infs->fetchAll();
+
+		$get_infs = $this->bdd->query('SELECT * FROM '.$this->args[0]);
+		$tab_infs = $get_infs->fetchAll();
+
+		print_r($this->get_just_val($colum, 'Field'));
+		echo "<br>";
+		print_r($this->get_just_val($tab_infs, ["int"]));
+
+		return ($this->create_table($this->get_just_val($colum, 'Field'), $this->get_just_val($tab_infs, ["int"]), 0));
+	}
+
+	private function get_just_val($tab, $champs) {
 		$i = 0;
-		while (isset($res[$i])) {
-			$tables[] = $res[$i][0];
+		$col = [];
+
+		while (isset($tab[$i])) {
+
+			if (is_array($champs)) {
+				foreach ($tab[$i] as $key => $value) {
+					if ($champs[0] == "int") {
+						if (is_numeric($key))
+							$col[] = $value;
+					}
+				}
+			} else
+				$col[] = $tab[$i][$champs];
 			$i++;
 		}
-
-		return ($this->create_table("Database table(s)", $tables, 0));
+		return ($col);
 	}
 
 	private function create_table($header, $values, $type) {
@@ -48,13 +90,7 @@ Class bdd {
 		else
 			$tbl->setHeaders([$header]);
 
-		foreach ($values as $key => $value) {
-
-			if ($type == 1)
-				$tbl->addRow([$key, $value]);
-			else
-				$tbl->addRow([$value]);
-		}
+		$tbl->addRow($values);
 
 		return ($tbl->getTable());
 	}
