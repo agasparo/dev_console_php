67e463cf13863ae3cca569ce3ee1aeee1ce9b87c
arthur <arthurysiite@gmail.Com>
2019-08-23 14:54:18 +0200

    hypertube

--- modules/bdd.module.php
+++ modules/bdd.module.php
@@ -12,13 +12,38 @@ Class bdd {
 		$this->args = $arguments;
 		$this->comm = $commande;
 
-		$path = new link('dev_console/.env');
+		$path = new link('.env');
 		$data = file($path->get_link(1));
 
 		if (!isset($data[2]))
 			$data[2] = "";
 
+		$data[1] = str_replace("\n", "", $data[1]);
+		$data[2] = str_replace("\n", "", $data[2]);
+		$data[3] = str_replace("\n", "", $data[3]);
+
 		$this->bdd = new PDO(str_replace("\n", "", $data[0]), str_replace("\n", "", $data[1]), $data[2]);
+	}
+
+	private function show_table() {
+
+
+	}
+
+	private function create_table($header, $values) {
+		$tbl = new createtab();
+
+		if (is_array($header))
+			$tbl->setHeaders($header);
+		else
+			$tbl->setHeaders([$header]);
+
+		foreach ($values as $key => $value) {
+
+			$tbl->addRow([$key, $value]);
+		}
+
+		return ($tbl->getTable());
 	}
 
 	public function execute() {
