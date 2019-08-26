5f29d96b06be5c7ca712a1279eeb413778c4df3a
Arthur GASPAROTTO <agasparo@e1r7p10.42.fr>
2019-08-26 11:27:05 +0200

    hypertube

--- modules/git.module.php
+++ modules/git.module.php
@@ -14,7 +14,15 @@ Class git {
 
 	private function set_commit_name() {
 
-		
+		if (!isset($this->args[0]))
+			return ("Usage : git.set_commit_name ['new name']");
+
+		$path_env = new link('.env');
+		$env = file($path_env->get_link(1));
+
+		$env[3] = $this->args[0];
+		file_put_contents($path_env->get_link(1), implode("", $env));
+		return ("Commit name change success");
 	}
 
 	public function execute() {
