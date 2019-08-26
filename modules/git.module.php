fabb07a86ae303c89d666750116d4beb9c75a698
Arthur GASPAROTTO <agasparo@e1r7p10.42.fr>
2019-08-26 11:31:26 +0200

    hypertube

--- modules/git.module.php
+++ modules/git.module.php
@@ -20,14 +20,22 @@ Class git {
 		$path_env = new link('.env');
 		$env = file($path_env->get_link(1));
 
-		$env[3] = $this->args[0];
+		$env[4] = $this->args[0];
 		file_put_contents($path_env->get_link(1), implode("", $env));
 		return ("Commit name change success");
 	}
 
 	private function push() {
 
-		
+		if (!isset($this->args[0]))
+			return ("Usage : git.push [directory]");
+
+		$path_env = new link('.env');
+		$env = file($path_env->get_link(1));
+
+		exec("cd ".$this->args[0]." && git add .");
+		exec("cd ".$this->args[0]." && git commit -m '".$env[4]."'");
+		exec("cd ".$this->args[0]." && git push");	
 	}
 
 	public function execute() {
--- require/to_include.php
+++ require/to_include.php
@@ -2,6 +2,7 @@
 require '/opt/lampp/htdocs/www/dev_console/modules/bdd.module.php';
 require '/opt/lampp/htdocs/www/dev_console/modules/change_header.module.php';
 require '/opt/lampp/htdocs/www/dev_console/modules/file.module.php';
+require '/opt/lampp/htdocs/www/dev_console/modules/git.module.php';
 require '/opt/lampp/htdocs/www/dev_console/modules/http_request.module.php';
 require '/opt/lampp/htdocs/www/dev_console/modules/style.module.php';
 require '/opt/lampp/htdocs/www/dev_console/modules/system.module.php';
