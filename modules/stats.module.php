ecca3e622ac1d05b4737bde79e87fdf7dcbf1a7a
Arthur GASPAROTTO <agasparo@e1r7p10.42.fr>
2019-08-26 15:08:19 +0200

    hypertube

--- modules/stats.module.php
+++ modules/stats.module.php
@@ -3,13 +3,18 @@
 Class stats {
 
 	private $comm;
-	private $commandes = ["", "", "", "", ""];
+	private $commandes = ["most", "less", "moyenne", "", ""];
 	private $args = [];
 
 	public function __Construct($commande, $arguments) {
 
 		$this->args = $arguments;
 		$this->comm = $commande;
+	}
+
+	private function check_require() {
+
+
 	}
 
 	public function execute() {
