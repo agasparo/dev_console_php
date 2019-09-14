333f324ac2c941c11f933f0d2731e2e6415d3f54
arthur <arthurysiite@gmail.Com>
2019-09-14 09:34:34 +0200

    hypertube

--- modules/system.module.php
+++ modules/system.module.php
@@ -16,7 +16,7 @@ Class system {
 	/**
 	 * @commandes Array
 	 */
-	private $commandes = ["reload_modules", "clear", "model_module"];
+	private $commandes = ["reload_modules", "clear", "model_module", "server_infos", "server_get_info"];
 
 	public function __Construct(String $commande, Array $arguments) {
 
@@ -40,6 +40,29 @@ Class system {
 
 		$path_env = new link('template/module_type.txt');
 		return (file_get_contents($path_env->get_link(1)));
+	}
+
+	private function server_infos() : String {
+
+		$str = "";
+
+		foreach ($_SERVER as $key => $value) {
+			
+			$str .= $key.": ".$value."<br>";
+		}
+
+		return ($str);
+	}
+
+	public function server_get_info() : String {
+
+		if (!isset($this->args[0]) || empty($this->args[0]))
+			return ("Usage : system.server_get_info [info to search]");
+
+		if (!isset($_SERVER[strtoupper($this->args[0])]))
+			return ("The info '".$this->args[0]."' doesn't exist");
+
+		return ($_SERVER[strtoupper($this->args[0])]);
 	}
 
 	public function execute() : String {
