1fe4ab733a56320c5165abbc321aeff94d7d234e
Lester PELISSIER <lpelissi@e1r8p10.42.fr>
2019-09-16 12:07:07 +0200

    hypertube

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-faire le readme correct
+This script is made by arthur gasparotto
+
+
+To reload all modules do : system.reload_modules
+
+To use module commande do : {module}.{command} [arguments ...]
+
+Modules description:
+
+BDD module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+To add module juste put the module file in the directory modules
+if file name is module_type.module.php
+the classname must be like : module_type
+The file name must be like : name.module.php
--- require/to_include.php
+++ require/to_include.php
@@ -1,11 +1,11 @@
 <?php
-require '/Users/arthur/server/dev_console/modules/bdd.module.php';
-require '/Users/arthur/server/dev_console/modules/change_header.module.php';
-require '/Users/arthur/server/dev_console/modules/file.module.php';
-require '/Users/arthur/server/dev_console/modules/git.module.php';
-require '/Users/arthur/server/dev_console/modules/http_request.module.php';
-require '/Users/arthur/server/dev_console/modules/stats.module.php';
-require '/Users/arthur/server/dev_console/modules/style.module.php';
-require '/Users/arthur/server/dev_console/modules/system.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/bdd.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/change_header.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/file.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/git.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/http_request.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/stats.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/style.module.php';
+require '/Users/lpelissi/Applications/mamp7.2/apache2/htdocs/dev_console/modules/system.module.php';
 
 ?>
